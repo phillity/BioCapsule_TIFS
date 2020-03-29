c5c109e31934dc94a7bb120b1e67d747849173ab
Tyler Phillips <phillity@umail.iu.edu>
2020-03-28 23:14:54 -0400

    update resize

--- src/extract.py
+++ src/extract.py
@@ -7,6 +7,42 @@ from argparse import ArgumentParser
 from model.face_model import FaceNet, ArcFace
 
 
+def resize(image, image_size=320, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # image height and width are the same
+    if h == w:
+        return cv2.resize(image, (image_size, image_size), interpolation=inter)
+
+    # if image height is greater than height
+    elif h > w:
+        r = image_size / float(h)
+        dim = (int(w * r), image_size)
+        resized = cv2.resize(image, dim, interpolation=inter)
+        if (image_size - resized.shape[1]) % 2 == 0:
+            pad = int((image_size - resized.shape[1]) / 2)
+            return cv2.copyMakeBorder(resized, 0, 0, pad, pad, cv2.BORDER_CONSTANT, 0)
+        else:
+            pad = int((image_size - resized.shape[1]) / 2)
+            return cv2.copyMakeBorder(resized, 0, 0, pad, pad + 1, cv2.BORDER_CONSTANT, 0)
+
+    # if image width is greater than height
+    else:
+        r = image_size / float(w)
+        dim = (image_size, int(h * r))
+        resized = cv2.resize(image, dim, interpolation=inter)
+        if (image_size - resized.shape[0]) % 2 == 0:
+            pad = int((image_size - resized.shape[0]) / 2)
+            return cv2.copyMakeBorder(resized, pad, pad, 0, 0,  cv2.BORDER_CONSTANT, 0)
+        else:
+            pad = int((image_size - resized.shape[0]) / 2)
+            return cv2.copyMakeBorder(resized, pad, pad + 1, 0, 0,  cv2.BORDER_CONSTANT, 0)
+
+
+
 def progress_bar(text, percent, barLen=20):
     print("{} -- [{:<{}}] {:.0f}%".format(
         text, "=" * int(barLen * percent), barLen, percent * 100), end="\r"
@@ -54,6 +90,7 @@ def extract(dataset, method, gpu):
         for image_fi in os.listdir(os.path.join(os.path.abspath(""), "image", dataset, subject_dir)):
             image = cv2.imread(os.path.join(
                 os.path.abspath(""), "image", dataset, subject_dir, image_fi))
+            image = resize(image)
 
             X[cnt] = model.embed(image)
             y[cnt] = subject_y + 1
