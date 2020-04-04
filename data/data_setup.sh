8cadccc2a087e541da5dfde5478d6540d67d2067
Tyler Phillips <phillity@umail.iu.edu>
2020-04-03 20:05:03 -0400

    Delete tar archives after download

--- data/data_setup.sh
+++ data/data_setup.sh
@@ -5,7 +5,9 @@ cd data
 echo "Downloading and extracting FaceNet features!"
 wget https://cs.iupui.edu/~phillity/facenet_tifs.tar.gz
 tar -xvf facenet_tifs.tar.gz
+rm facenet_tifs.tar.gz
 
 echo "Downloading and extracting ArcFace features!"
 wget https://cs.iupui.edu/~phillity/arcface_tifs.tar.gz
 tar -xvf arcface_tifs.tar.gz
+rm arcface_tifs.tar.gz
